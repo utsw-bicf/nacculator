284ecf3887a19e03f5fd320ceb55422fa2cf0931
Jason Hough <jason.hough@utsouthwestern.edu>
2020-06-01 15:59:48 -0500

    uses headers from modified dictionary

--- nacc/redcap2json.py
+++ nacc/redcap2json.py
@@ -30,30 +30,34 @@ def convert_to_json(options):
         nrows = 10  # TODO: Remove nrows param when done testing
     )
 
-    # Map (replace) each column's values to the corresponding schema values for that header
+    # Replace each row/column value (cell) to the corresponding schema value for that header
     ivp_a1_schema_dict = ivp_a1()
     for headerIndex, header in enumerate(all_csv):
         for valueIndex, value in enumerate(all_csv[header]):
             # Ensure header exists in dictionary
             if ivp_a1_schema_dict.get(header) is not None:
                 # Ensure the current value for the current header is valid
-                if str(value) == 'nan' or ivp_a1_schema_dict[header].get(value) is None:
-                    # all_csv.iat[valueIndex, headerIndex] = ""
-                    pass
-                else:
+                if str(value) != 'nan' and ivp_a1_schema_dict[header].get(value) is not None:
                     all_csv.iat[valueIndex, headerIndex] = str(value + ' ' + ivp_a1_schema_dict[header][value])
 
+    # Create a list of subset headers using the form schema headers (dictionary)
+    ivp_a1_headers = list(ivp_a1_schema_dict.keys())
 
-    # TODO: Use headers from forms.py and only map the values that have keys that exist in the dictionary
-    #       There are some headers that do not have mapped values (i.e. user entered).
-    #       Therefore, cannot rely on only the ivp_a1 dictionary unless it takes this into account.
+    # Ensure all headers from dictionary exist in the CSV file
+    # If not, remove them from the headers subset list
+    for header in ivp_a1_headers:
+        if all_csv.get(header) is None:
+            ivp_a1_headers.remove(header)
 
-    # Create subset dataframes for each form using the form schema headers
-    ivp_a1_headers = ivp_a1_schema_dict.keys()
+    # Create the subset dataframe for the specific form
     ivp_a1_csv = all_csv[ivp_a1_headers]
 
     # Drop all fully empty records (rows)
-    ivp_a1_csv.dropna(how = 'all', inplace = True)
+    ivp_a1_csv = ivp_a1_csv.dropna(how = 'all')
+
+    # Fill any remaining empty cells with empty strings
+    # These would be cells in which its entire row was not empty
+    ivp_a1_csv = ivp_a1_csv.fillna(value = "")
 
     # Convert form to JSON
     ivp_a1_json = ivp_a1_csv.to_json(
@@ -62,7 +66,7 @@ def convert_to_json(options):
     )
 
     # Create and write to output JSON file
-    output_file_path = os.path.join(os.getcwd(), 'ivp_a1.json')
+    output_file_path = os.path.join(os.getcwd(), 'output_ivp_a1.json')
     ivp_a1_json_file = open(output_file_path, 'w')
     ivp_a1_json_file.write(ivp_a1_json)
     ivp_a1_json_file.close()
