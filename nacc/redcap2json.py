a361e7f4629e07d29feff049aef47e63c16ba97f
Jason Hough <jason.hough@utsouthwestern.edu>
2020-06-01 13:42:09 -0500

    drops empty records

--- nacc/redcap2json.py
+++ nacc/redcap2json.py
@@ -30,7 +30,7 @@ def convert_to_json(options):
         nrows = 10  # TODO: Remove nrows param when done testing
     )
 
-    # Map each column's values to the corresponding schema values for that header
+    # Map (replace) each column's values to the corresponding schema values for that header
     ivp_a1_schema_dict = ivp_a1()
     for headerIndex, header in enumerate(all_csv):
         for valueIndex, value in enumerate(all_csv[header]):
@@ -38,29 +38,31 @@ def convert_to_json(options):
             if ivp_a1_schema_dict.get(header) is not None:
                 # Ensure the current value for the current header is valid
                 if str(value) == 'nan' or ivp_a1_schema_dict[header].get(value) is None:
-                    # all_csv.iat[valueIndex, headerIndex] = ivp_a1_schema_dict[header]['nan']
+                    # all_csv.iat[valueIndex, headerIndex] = ""
                     pass
                 else:
                     all_csv.iat[valueIndex, headerIndex] = str(value + ' ' + ivp_a1_schema_dict[header][value])
 
+
+    # TODO: Use headers from forms.py and only map the values that have keys that exist in the dictionary
+    #       There are some headers that do not have mapped values (i.e. user entered).
+    #       Therefore, cannot rely on only the ivp_a1 dictionary unless it takes this into account.
+
     # Create subset dataframes for each form using the form schema headers
-    ivp_a1_headers = ivp_a1().keys()
+    ivp_a1_headers = ivp_a1_schema_dict.keys()
     ivp_a1_csv = all_csv[ivp_a1_headers]
 
-    # Convert each form to JSON
+    # Drop all fully empty records (rows)
+    ivp_a1_csv.dropna(how = 'all', inplace = True)
+
+    # Convert form to JSON
     ivp_a1_json = ivp_a1_csv.to_json(
         orient = 'records',
         indent = 2
     )
 
-    # Convert entire CSV to JSON
-    all_json = all_csv.to_json(
-        orient = 'records',
-        indent = 2
-    )
-
     # Create and write to output JSON file
-    output_file_path = os.path.join(os.getcwd(), 'output.json')
+    output_file_path = os.path.join(os.getcwd(), 'ivp_a1.json')
     ivp_a1_json_file = open(output_file_path, 'w')
     ivp_a1_json_file.write(ivp_a1_json)
     ivp_a1_json_file.close()
